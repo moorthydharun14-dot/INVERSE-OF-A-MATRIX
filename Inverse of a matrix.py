78da92c8c8ade79f6af3f8749c0582323b22c050
moorthydharun14-dot <moorthydharun14@gmail.com>
2026-02-23 23:01:32 +0530

    Delete Inverse of a matrix.py

--- Inverse of a matrix.py
+++
@@ -1,7 +0,0 @@
-#Program to find the inverse of a matrix.
-#RegisterNumber: DHARUN M 25018453
-#RegisterNumber:212225230057
-import numpy as np
-matrix = np.array([[6, 2, 3], [3, 1, 1],[10, 3, 4]])
-rank = np.linalg.inv(matrix)
-print(rank)
